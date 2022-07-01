1fe703755e50a5319db2eef30a4025dac83dd6be
bocacha <martineduardogonzalez@gmail.com>
2022-06-30 21:30:00 -0400

    feat: Adding user middlewares & refactoring store

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,5 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
 import products from '../middlewares/Products';
+import users from '../middlewares/Users';
 
 export default [
   products,
+  users,
+  
 ];
