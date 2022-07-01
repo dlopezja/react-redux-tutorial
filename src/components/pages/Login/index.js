0fc88a85db950cd931746649f3c192078be86279
Martín <blackhack1@hotmail.com>
2022-06-30 23:50:56 -0300

    adding nord color pallete dark theme, fixing minor bugs

--- src/components/pages/Login/index.js
+++ src/components/pages/Login/index.js
@@ -5,18 +5,14 @@ import { connect } from 'react-redux';
 import actions from '../../../actions/Users';
 
 function LoginPage(props){
-    
 
         // reset login status
-        
 
         const state = {
             username: '',
             password: '',
             submitted: false,
         };
-
-        
 
     function handleChange(e) {
         const { name, value } = e.target;
@@ -32,31 +28,31 @@ function LoginPage(props){
             this.props.login(username, password);
         }
     }
-
     
-         const { loggingIn } = this.props;
-        const { username, password, submitted } = this.state;
+        const { loggingIn } = props;
+        const { username, password, submitted } = state;
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h2>Login</h2>
-                <form name="form" onSubmit={this.handleSubmit}>
+                <form name="form" onSubmit={handleSubmit}>
                     <div className={'form-group' + (state.submitted && !state.username ? ' has-error' : '')}>
                         <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control" name="username" value={state.username} onChange={this.handleChange} />
+                        <input type="text" className="form-control" name="username" value={state.username} onChange={handleChange} />
                         {state.submitted && !state.username &&
                             <div className="help-block">Username is required</div>
                         }
                     </div>
                     <div className={'form-group' + (state.submitted && !state.password ? ' has-error' : '')}>
                         <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password" value={state.password} onChange={this.handleChange} />
+                        <input type="password" className="form-control" name="password" value={state.password} onChange={handleChange} />
                         {state.submitted && !state.password &&
                             <div className="help-block">Password is required</div>
                         }
                     </div>
                     <div className="form-group">
                         <button className="btn btn-primary">Login</button>
-                        {loggingIn &&
+                        {
+                        // loggingIn &&
                             <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
                         }
                         <Link to="/register" className="btn btn-link">Register</Link>
@@ -66,10 +62,9 @@ function LoginPage(props){
         );
     }
 
-
 function mapState(state) {
     // const { loggingIn } = state.authentication;
-    const { loggingIn } =true
+    const { loggingIn } = true
     return { loggingIn };
 }
 
