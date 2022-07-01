6631d89525a2bbcd2247b402ad642d4da4432f5a
Martín <blackhack1@hotmail.com>
2022-07-01 00:48:51 -0300

    feat: done adding nord theme color palette

--- src/components/pages/Login/index.js
+++ src/components/pages/Login/index.js
@@ -4,15 +4,17 @@ import { connect } from 'react-redux';
 
 import actions from '../../../actions/Users';
 
-function LoginPage(props){
+import Card from 'react-bootstrap/Card'
 
-        // reset login status
+function LoginPage(props) {
 
-        const state = {
-            username: '',
-            password: '',
-            submitted: false,
-        };
+    // reset login status
+
+    const state = {
+        username: '',
+        password: '',
+        submitted: false,
+    };
 
     function handleChange(e) {
         const { name, value } = e.target;
@@ -28,12 +30,15 @@ function LoginPage(props){
             this.props.login(username, password);
         }
     }
-    
-        const { loggingIn } = props;
-        const { username, password, submitted } = state;
-        return (
-            <div className="col-md-6 col-md-offset-3">
+
+    const { loggingIn } = props;
+    const { username, password, submitted } = state;
+    return (
+        <Card className="mt-3 mx-auto w-50">
+            <Card.Header>
                 <h2>Login</h2>
+            </Card.Header>
+            <Card.Body>
                 <form name="form" onSubmit={handleSubmit}>
                     <div className={'form-group' + (state.submitted && !state.username ? ' has-error' : '')}>
                         <label htmlFor="username">Username</label>
@@ -52,15 +57,16 @@ function LoginPage(props){
                     <div className="form-group">
                         <button className="btn btn-primary">Login</button>
                         {
-                        // loggingIn &&
+                            // loggingIn &&
                             <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
                         }
                         <Link to="/register" className="btn btn-link">Register</Link>
                     </div>
                 </form>
-            </div>
-        );
-    }
+            </Card.Body>
+        </Card>
+    );
+}
 
 function mapState(state) {
     // const { loggingIn } = state.authentication;
