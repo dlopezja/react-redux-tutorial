2a7621232a55c805dadf1650871825a37dde8b63
Cesar Illanes <cesarill99@gmail.com>
2022-07-03 23:00:15 -0400

    Minor changes

--- src/components/protectedRoute/index.js
+++ src/components/protectedRoute/index.js
@@ -3,7 +3,10 @@ import { Navigate, Outlet } from "react-router-dom";
 function ProtectedRoute({ user, redirectPath = '/', children}){
     if(!user){
         return(
-            <Navigate to={redirectPath} replace />
+            <>
+                <Navigate to={redirectPath} replace />
+                {alert("YOU SHALL NOT PASS!!!")}
+            </>
         );
     }
     return children ? children : <Outlet />;
--- src/index.js
+++ src/index.js
@@ -9,13 +9,11 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
