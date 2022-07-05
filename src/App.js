a8163557b93f1512adbcd6bc734dfda9340b2340
Luis <luis.becerra.15.12@gmail.com>
2022-07-05 12:59:38 -0300

    merging

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import MainRouter from './router';
 
-
 function App() {
   //window.localStorage.clear();
+  console.log('api_url= ' + process.env.REACT_APP_API_URL);
   return (
     <div className="App">
       <BrowserRouter>
--- src/utils/apiFactory/index.js
+++ src/utils/apiFactory/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function getBaseAxiosInstance() {
   return axios.create({
-    baseURL: 'http://localhost:8080/',
+    baseURL: process.env.REACT_APP_API_URL,
     headers: {},
   });
 }
